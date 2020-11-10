50bb136f7af34a99dc5020cd9e914a43f85abaae
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-10 16:35:14 +0800

    testcase for user model\view\urls

--- zanhu/users/tests/test_models.py
+++ zanhu/users/tests/test_models.py
@@ -1,9 +1,18 @@
-import pytest
-
-from zanhu.users.models import User
-
-pytestmark = pytest.mark.django_db
+from test_plus.test import TestCase
 
 
-def test_user_get_absolute_url(user: User):
-    assert user.get_absolute_url() == f"/users/{user.username}/"
+class TestUser(TestCase):
+
+    def setUp(self):
+        self.user = self.make_user()
+
+    def test__str__(self):
+        self.assertEqual(self.user.__str__(), 'testuser')
+
+    def test_get_profile_name(self):
+        assert self.user.get_profile_name() == 'testuser'
+        self.user.nickname = '昵称'
+        assert self.user.get_profile_name() == '昵称'
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.user.get_absolute_url(), '/users/testuser')
