8a9a3f9e6acd8b5f17b62b6a9fef95b802c3ddfc
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-10 16:36:30 +0800

    testcase for user model\view\urls

--- zanhu/users/tests/test_urls.py
+++ zanhu/users/tests/test_urls.py
@@ -1,24 +1,20 @@
-import pytest
-from django.urls import resolve, reverse
-
-from zanhu.users.models import User
-
-pytestmark = pytest.mark.django_db
+from test_plus.test import TestCase
+from django.urls import reverse, resolve
 
 
-def test_detail(user: User):
-    assert (
-        reverse("users:detail", kwargs={"username": user.username})
-        == f"/users/{user.username}/"
-    )
-    assert resolve(f"/users/{user.username}/").view_name == "users:detail"
+class TestUserURLs(TestCase):
 
+    def setUp(self):
+        self.user = self.make_user()
 
-def test_update():
-    assert reverse("users:update") == "/users/~update/"
-    assert resolve("/users/~update/").view_name == "users:update"
+    def test_detail_reverse(self):
+        self.assertEqual(reverse('users:detail', kwargs={'username': 'testuser'}), '/users/testuser/')
 
+    def test_detail_resolve(self):
+        self.assertEqual(resolve('/users/testuser/').view_name, 'users:detail')
 
-def test_redirect():
-    assert reverse("users:redirect") == "/users/~redirect/"
-    assert resolve("/users/~redirect/").view_name == "users:redirect"
+    def test_update_reserve(self):
+        self.assertEqual(reverse('users:update'), '/users/update/')
+
+    def test_update_resolve(self):
+        self.assertEqual(resolve('/users/update/').view_name, 'users:update')
