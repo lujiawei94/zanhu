c3859fdde440d336c6bc89f5cc456da7bcf88ea2
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-11 17:08:48 +0800

    news config/urls

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("users/", include("zanhu.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path('news/', include('zanhu.news.url', namespace='news')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
