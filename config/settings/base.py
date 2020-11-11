9b81b6e4c7ee8fa48d535fd0c36faeadabaf092f
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-11 17:07:14 +0800

    news/base

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,6 +82,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "zanhu.users.apps.UsersConfig",
+    "zanhu.news.apps.NewsConfig"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
