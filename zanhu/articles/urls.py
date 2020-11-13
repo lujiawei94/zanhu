7bb8b379e93840441811b0ffcfd6900046de00ed
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-13 17:34:33 +0800

    articles/urls.py

---
+++ zanhu/articles/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from zanhu.articles import views
+
+app_name = 'articles'
+
+urlpatterns = [
+    path('', views.ArticlesListView.as_view(), name='list'),
+    path('write-new-article/', views.ArticleCreateView.as_view(), name='write_new'),
+    path('drafts/', views.DraftListView.as_view(), name='drafts'),
+]
