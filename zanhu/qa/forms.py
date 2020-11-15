1425cb02b9c9bed99a18cba16ddff6484b609096
lujiawei <lujiawei_0116@163.com>
2020-11-15 22:55:02 +0800

    apps/qa2

--- zanhu/qa/forms.py
+++ zanhu/qa/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from markdownx.fields import MarkdownxFormField
 
-from zanhu.articles.models import Article
+from zanhu.qa.models import Question
 
-class ArticleForm(forms.ModelForm):
 
-    content = MarkdownxFormField()
+class QuestionForm(forms.ModelForm):
     status = forms.CharField(widget=forms.HiddenInput())  # 隐藏
-    edited = forms.BooleanField(widget=forms.HiddenInput(), initial=False, required=False)
+    content = MarkdownxFormField()
+
     class Meta:
-        model = Article
-        fields = ['title', 'content', 'image', 'tags', 'status', 'edited']
+        model = Question
+        fields = ['title', 'content', 'tags', 'status']
--- zanhu/qa/urls.py
+++ zanhu/qa/urls.py
@@ -5,5 +5,9 @@ from zanhu.qa import views
 app_name = 'qa'
 
 urlpatterns = [
+    path('', views.UnansweredQuestionListView.as_view(), name='unanswered_q'),
+    path('answered/', views.AnsweredQuestionListView.as_view(), name='answered_q'),
     path('indexed/', views.QuestionListView.as_view(), name='all_q'),
+    path('ask-question/', views.CreateQuestionView.as_view(), name='ask_question'),
+    path('question-detail/<int:pk>/', views.QuestionDetailView.as_view(), name='question_detail'),
 ]
