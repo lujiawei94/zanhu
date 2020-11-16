e83ed1f6e303820c727633bcabc0f41958d67627
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-16 17:05:00 +0800

    qa/url

--- zanhu/qa/urls.py
+++ zanhu/qa/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('indexed/', views.QuestionListView.as_view(), name='all_q'),
     path('ask-question/', views.CreateQuestionView.as_view(), name='ask_question'),
     path('question-detail/<int:pk>/', views.QuestionDetailView.as_view(), name='question_detail'),
+    path('propose-answer/<int:question_id>/', views.CreateQuestionView.as_view(), name='propose_answer'),
+    path('question/vote/', views.question_vote, name='question_vote'),
+    path('answer/vote/', views.answer_vote, name='answer_vote'),
+    path('accept-answer/', views.accept_answer, name='accept_answer'),
 ]
