9f6fb7b973c295cc58a03f5e241916456a6d16ac
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-16 17:04:04 +0800

    base

--- config/settings/base.py
+++ config/settings/base.py
@@ -217,7 +217,7 @@ FIXTURE_DIRS = (str(APPS_DIR / "fixtures"),)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
 SESSION_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
-CSRF_COOKIE_HTTPONLY = False
+CSRF_COOKIE_HTTPONLY = False  # 默认为F，如果设置为T，前端js无法获取到csrf cookie
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
