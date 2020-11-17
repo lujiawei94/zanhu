029292f7f093a7b1e63b2ead07ef4c79e8f05779
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-17 14:12:59 +0800

    qa/test_views

--- zanhu/qa/tests/test_views.py
+++ zanhu/qa/tests/test_views.py
@@ -87,35 +87,28 @@ class TestCreateQuestionView(BaseQATest):
         setattr(request, '_messages', messages)
 
         response = self.get(views.CreateQuestionView, request=request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
         assert response.url == '/qa/'
 
-
-
-'''
 class TestQuestionDetailView(BaseQATest):
-    """测试问题详情"""
 
-    def get_context_data(self):
-        response = self.get(views.QuestionDetailView, request=self.request,
-                            pk=self.question_one.id)
-        self.response_200(response)
+    def test_get_context_data(self):
+        response = self.get(views.QuestionDetailView, request=self.request, pk=self.question_one.id)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context_data['question'], self.question_one)
 
 
 class TestCreateAnswerView(BaseQATest):
-    """测试创建回答"""
-
     def test_get(self):
-        response = self.get(views.CreateAnswerView, request=self.request,
-                            question_id=self.question_one.id)
-        self.response_200(response)
+        response = self.get(views.CreateAnswerView, request=self.request, question_id=self.question_one.id)
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, '编辑')
         self.assertContains(response, '预览')
         self.assertIsInstance(response.context_data['view'], views.CreateAnswerView)
 
-    def get_post(self):
-        request = RequestFactory().post('/fake-url', data={'content': 'content'})
+    def test_post(self):
+        data = {'content': 'content'}
+        request = RequestFactory().post('/fake-url', data=data)
         request.user = self.user
         # RequestFactory测试含有django.contrib.messages的视图 https://code.djangoproject.com/ticket/17971
         setattr(request, 'session', 'session')
@@ -123,68 +116,53 @@ class TestCreateAnswerView(BaseQATest):
         setattr(request, '_messages', messages)
 
         response = self.post(views.CreateAnswerView, request=request)
-        assert response.status_code == 302
+        self.assertEqual(response.status_code, 302)
         assert response.url == f'/qa/question-detail/{self.question_one.id}'
-
 
 class TestQAVote(BaseQATest):
 
-    def setUp(self):
+    def setUP(self):
         super(TestQAVote, self).setUp()
         self.request = RequestFactory().post('/fake-url', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        # QueryDict instance is immutable, request.POST是QueryDict对象，不可变
-        self.request.POST = self.request.POST.copy()
-
+        self.request.POST = self.request.POST.copy()  # 对于不可变对象的处理方式
         self.request.user = self.other_user
 
     def test_question_upvote(self):
-        """赞同问题"""
         self.request.POST['question'] = self.question_one.id
         self.request.POST['value'] = 'U'
-
-        response = views.question_vote(self.request)
-
-        assert response.status_code == 200
+        response = self.post(views.question_vote, request=self.request)
+        self.assertEqual(response.status_code, 200)
         assert json.loads(response.content)['votes'] == 1
 
     def test_question_downvote(self):
-        """反对问题"""
         self.request.POST['question'] = self.question_two.id
         self.request.POST['value'] = 'D'
-
-        response = views.question_vote(self.request)
-
+        response = self.post(views.question_vote, request=self.request)
         assert response.status_code == 200
         assert json.loads(response.content)['votes'] == -1
 
     def test_answer_upvote(self):
-        """赞同问答"""
         self.request.POST['answer'] = self.answer.uuid_id
         self.request.POST['value'] = 'U'
-
-        response = views.answer_vote(self.request)
-
+        response = self.post(views.answer_vote, request=self.request)
         assert response.status_code == 200
         assert json.loads(response.content)['votes'] == 1
 
     def test_answer_downvote(self):
-        """反对回答"""
         self.request.POST['answer'] = self.answer.uuid_id
         self.request.POST['value'] = 'D'
-
-        response = views.answer_vote(self.request)
-
+        response = self.post(views.answer_vote, request=self.request)
         assert response.status_code == 200
         assert json.loads(response.content)['votes'] == -1
 
+
     def test_accept_answer(self):
         """接受回答"""
-
         self.request.user = self.user  # self.user是提问者
         self.request.POST['answer'] = self.answer.uuid_id
 
-        response = views.accept_answer(self.request)
+        response = views.accept_answer(self.request)  # 等同于 response = self.post(views.accept_answer, request=self.request)
 
         assert response.status_code == 200
         assert json.loads(response.content)['status'] == 'true'
-'''
+
