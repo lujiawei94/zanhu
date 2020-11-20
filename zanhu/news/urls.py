c8aedad4813fee998a737d4d47b29522a8dabea3
lujiawei94 <49287136+lujiawei94@users.noreply.github.com>
2020-11-20 17:19:29 +0800

    Create urls.py

--- zanhu/news/urls.py
+++ zanhu/news/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('like/', views.like, name='like_post'),
     path('get-thread/', views.get_thread, name='get_thread'),
     path('post-comment/', views.post_comment, name='post_comments'),
+    path('update-interactions/', views.update_interactions, name='update_interactions'),
 ]
